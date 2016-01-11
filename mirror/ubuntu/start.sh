5cb7542e62499d5a82ab5fe132283817df0e61ea
Antoine GIRARD <sapk@users.noreply.github.com>
2016-01-11 15:15:29 +0100

    Create start.sh

---
+++ mirror/ubuntu/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Based on https://wiki.ubuntu.com/Mirrors/Scripts for archive
+
+# Find a source mirror near you which supports rsync on
+# https://launchpad.net/ubuntu/+archivemirrors
+# rsync://<iso-country-code>.rsync.archive.ubuntu.com/ubuntu should always work
+RSYNCSOURCE=${RSYNCSOURCE:-"rsync://rsync.archive.ubuntu.com/ubuntu"}
+  
+# Define where you want the mirror-data to be on your mirror
+BASEDIR=${BASEDIR:-"/var/www/ubuntuarchive/"}
+  
+fatal() {
+  echo "$1"
+  exit 1
+}
+
+sync() {
+  
+  rsync --recursive --times --links --hard-links \
+    --stats \
+    --exclude "Packages*" --exclude "Sources*" \
+    --exclude "Release*" \
+    ${RSYNCSOURCE} ${BASEDIR} || fatal "First stage of sync failed."
+  
+  rsync --recursive --times --links --hard-links \
+    --stats --delete --delete-after \
+    ${RSYNCSOURCE} ${BASEDIR} || fatal "Second stage of sync failed."
+  
+  date -u > ${BASEDIR}/project/trace/$(hostname -f)
+  
+  sleep 3h
+  
+  sync
+}
+
+darkhttpd ${BASEDIR} --port 8080 --chroot & 
+
+sync
+
