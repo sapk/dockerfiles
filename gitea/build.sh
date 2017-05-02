dace788df931a0efe40f188af14eb1c7af6d0a7d
Antoine GIRARD <sapk@users.noreply.github.com>
2017-05-02 21:00:59 +0200

    Update build.sh

--- gitea/build.sh
+++ gitea/build.sh
@@ -3,13 +3,11 @@
 cd /tmp
 #Build last docker version. Based on: http://blog.alexellis.io/mutli-stage-docker-builds/#howcanitryitout
 git clone https://github.com/docker/docker 
-cd docker
-make tgz
+(cd docker && make install)
 
 git clone https://github.com/sapk-fork/gitea
 cd gitea
 git checkout multi-stage-docker
 #sed -i "s;gitea/;sapk/;g" docker/manifest/gitea*
-cd ..
-
-docker run -v `pwd`/gitea:/gitea -v `pwd`/bundles:/go/src/github.com/docker/docker/bundles --privileged -ti docker-dev:master bash -c "export PATH=\$PATH:\$(pwd)/bundles/latest/dynbinary-daemon:\$(pwd)/bundles/latest/binary-client/; cd /gitea; dockerd & make docker-multi-update-all;"
+dockerd & 
+make docker-multi-update-all
