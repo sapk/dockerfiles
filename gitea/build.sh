80ca73d010549a1159e18e0a61a286935c773488
Antoine GIRARD <sapk@users.noreply.github.com>
2017-05-16 01:50:12 +0200

    Set to sapk/

--- gitea/build.sh
+++ gitea/build.sh
@@ -5,11 +5,11 @@ cd /tmp
 #Not needed since docker as release this function
 #git clone https://github.com/docker/docker 
 #(cd docker && sudo make install)
-
+#service docker restart
 
 git clone https://github.com/sapk-fork/gitea
 cd gitea
 git checkout multi-stage-docker
-#sed -i "s;gitea/;sapk/;g" docker/manifest/gitea*
-service docker restart
-make docker-multi-update-all
+sed -i "s;gitea/;sapk/;g" docker/manifest/gitea*
+
+DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
