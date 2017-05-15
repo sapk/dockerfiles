284a793a5bed57bea9c4afac5c00bcdaa468b879
Antoine GIRARD <sapk@users.noreply.github.com>
2017-05-16 01:38:57 +0200

    Update build.sh

--- gitea/build.sh
+++ gitea/build.sh
@@ -2,8 +2,10 @@
 
 cd /tmp
 #Build last docker version. Based on: http://blog.alexellis.io/mutli-stage-docker-builds/#howcanitryitout
-git clone https://github.com/docker/docker 
-(cd docker && sudo make install)
+#Not needed since docker as release this function
+#git clone https://github.com/docker/docker 
+#(cd docker && sudo make install)
+
 
 git clone https://github.com/sapk-fork/gitea
 cd gitea
