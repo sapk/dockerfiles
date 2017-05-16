abcd21bf95951b280a0d7061ce1c88c8cb02333e
Antoine GIRARD <sapk@users.noreply.github.com>
2017-05-16 02:54:43 +0200

    Update build.sh

--- gitea/build.sh
+++ gitea/build.sh
@@ -12,14 +12,17 @@ cd gitea
 git checkout multi-stage-docker
 #sed -i "s;gitea/;sapk/;g" docker/manifest/gitea*
 
-DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-amd64 docker-multi-arm docker-multi-arm64
+DOCKER_MANIFEST=docker/manifest/gitea.yml DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-manifest
 
-GITEA_VERSION=v1.1.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-GITEA_VERSION=v1.1.0 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-GITEA_VERSION=v1.0.2 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-GITEA_VERSION=v1.0.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-GITEA_VERSION=v1.0.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+GITEA_VERSION=v1.1.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-amd64 docker-multi-arm docker-multi-arm64
+DOCKER_MANIFEST=docker/manifest/gitea-1-1-1.yml DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-manifest
+
+#GITEA_VERSION=v1.1.0 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+#GITEA_VERSION=v1.0.2 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+#GITEA_VERSION=v1.0.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+#GITEA_VERSION=v1.0.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
 
 
-GITEA_VERSION=release/v1.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-GITEA_VERSION=release/v1.0 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+#GITEA_VERSION=release/v1.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+#GITEA_VERSION=release/v1.0 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
