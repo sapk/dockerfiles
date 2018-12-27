98175ff8cad8421b211a29044b35f032850a38bf
Antoine GIRARD <sapk@users.noreply.github.com>
2018-12-27 17:33:05 +0100

    gitea: change docker image name to sapk/gitea

--- gitea/build.sh
+++ gitea/build.sh
@@ -5,9 +5,9 @@ cd gitea
 git fetch origin pull/3958/head:docker-multiarch 
 git checkout docker-multiarch
 
-TARGET=amd64 GOARCH=amd64 QEMU_ARCH=amd64 make docker-cross
-TARGET=arm64v8 GOARCH=arm64 QEMU_ARCH=aarch64 make docker-cross
-TARGET=arm32v6 GOARCH=arm QEMU_ARCH=arm make docker-cross
+DOCKER_IMAGE=sapk/gitea TARGET=amd64 GOARCH=amd64 QEMU_ARCH=amd64 make docker-cross
+DOCKER_IMAGE=sapk/gitea TARGET=arm64v8 GOARCH=arm64 QEMU_ARCH=aarch64 make docker-cross
+DOCKER_IMAGE=sapk/gitea TARGET=arm32v6 GOARCH=arm QEMU_ARCH=arm make docker-cross
 
 docker push sapk/gitea:latest-linux-amd64 
 docker push sapk/gitea:latest-linux-arm 
