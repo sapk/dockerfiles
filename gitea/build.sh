a36c99ddcb826244012c2312fd6064380041280b
Antoine GIRARD <sapk@users.noreply.github.com>
2018-12-27 16:48:31 +0100

    gitea: update multi-arch build steps

--- gitea/build.sh
+++ gitea/build.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-cd /tmp
-#Build last docker version. Based on: http://blog.alexellis.io/mutli-stage-docker-builds/#howcanitryitout
-#Not needed since docker as release this function
-#git clone https://github.com/docker/docker 
-#(cd docker && sudo make install)
-#service docker restart
-
-git clone https://github.com/sapk-fork/gitea --branch docker-multi-arch-base --single-branch 
+git clone https://github.com/go-gitea/gitea --depth 10
 cd gitea
+git fetch origin pull/3958/head:docker-multiarch 
+git checkout docker-multiarch
+
 DOCKER_IMAGE=sapk/gitea make docker-multi-arch-push-manifest
 
+GOARCH=amd64 QEMU_ARCH=amd64 make docker-generate-arch-dockerfile docker-download-qemu-binary
+GOARCH=arm QEMU_ARCH=arm make docker-generate-arch-dockerfile docker-download-qemu-binary
+GOARCH=arm64 QEMU_ARCH=aarch64 make docker-generate-arch-dockerfile docker-download-qemu-binary
 
+TARGET=amd64 docker build -f docker/Dockerfile.amd64 -t sapk/gitea:latest-linux-amd64 .
+TARGET=arm32v6 docker build -f docker/Dockerfile.arm -t sapk/gitea:latest-linux-arm .
+TARGET=arm64v8 docker build -f docker/Dockerfile.aarch64 -t sapk/gitea:latest-linux-arm64 .
 
-#Old CMD
-#DOCKER_PUSHIMAGE=sapk/gitea make --always-make docker-multi-amd64 
-#DOCKER_PUSHIMAGE=sapk/gitea make --always-make docker-multi-arm 
-#DOCKER_PUSHIMAGE=sapk/gitea make --always-make docker-multi-arm64
-#DOCKER_MANIFEST=docker/manifest/gitea.yml DOCKER_PUSHIMAGE=sapk/gitea make --always-make docker-multi-update-manifest
-#DOCKER_PUSHIMAGE=sapk/gitea GITEA_VERSION=v1.1.1 make --always-make docker-multi-update-all
-#DOCKER_MANIFEST=docker/manifest/gitea-1-1-1.yml DOCKER_PUSHIMAGE=sapk/gitea make -B docker-multi-update-manifest
-
-#GITEA_VERSION=v1.1.0 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-#GITEA_VERSION=v1.0.2 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-#GITEA_VERSION=v1.0.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-#GITEA_VERSION=v1.0.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-
-
-#GITEA_VERSION=release/v1.1 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
-#GITEA_VERSION=release/v1.0 DOCKER_PUSHIMAGE=sapk/gitea make docker-multi-update-all
+docker push apk/gitea:latest-linux-amd64 
+docker push apk/gitea:latest-linux-arm 
+docker push apk/gitea:latest-linux-arm64 
