6fd326be8dd27dd7d871f269bf2d3b614c7cd28e
Antoine GIRARD <sapk@users.noreply.github.com>
2019-10-31 23:22:18 +0100

    Update buildx.sh

--- gitea/buildx.sh
+++ gitea/buildx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Setup buildx
+export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
 docker build --platform=local -o . git://github.com/docker/buildx
 mv buildx ~/.docker/cli-plugins/docker-buildx
@@ -10,4 +11,4 @@ git clone https://github.com/go-gitea/gitea --depth 10
 cd gitea
 
 #Building
-docker buildx build .
+docker buildx build --push --platform=linux/amd64,linux/arm64,linux/arm/v6 .
